82778fbcbeb5b6f1c03749eba9abde34b4d838a6
namdq2 <namquoc.dev@gmail.com>
2021-05-01 19:57:34 +0700

    update database config

--- models/db.js
+++ models/db.js
@@ -8,13 +8,15 @@ let {
     MONGO_DB
 } = process.env;
 
-MONGO_USERNAME = MONGO_USERNAME ? MONGO_USERNAME : 'admin'
-MONGO_PASSWORD = MONGO_PASSWORD ? MONGO_PASSWORD : 'admin'
 MONGO_HOSTNAME = MONGO_HOSTNAME ? MONGO_HOSTNAME : 'localhost'
 MONGO_PORT = MONGO_PORT ? MONGO_PORT : '27017'
-MONGO_DB = MONGO_DB ? MONGO_DB : 'webnoibo'
+MONGO_DB = MONGO_DB ? MONGO_DB : 'webnoibo-database'
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+let url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+if (MONGO_USERNAME) {
+    url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+}
 
 const options = {
     useNewUrlParser: true,
